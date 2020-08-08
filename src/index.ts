158b5cb63cddb96dd496b714662013e0d343f797
Dong-Young Kim <31337.persona@gmail.com>
2020-08-09 01:32:02 +0900

    build(npm): add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { createConnection } from "typeorm";
+import * as cors from "cors";
 import UsersRouter from "./routes/Users";
 
 createConnection()
@@ -8,6 +9,7 @@ createConnection()
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
 
     // register express routes
     app.use("/users", UsersRouter);
